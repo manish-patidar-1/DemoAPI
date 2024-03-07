1a81d9030992914ece04c4a0b5a3cf7ccff5f59b
Patidar <Manish.Patidar@philips.com>
2024-03-07 12:11:03 +0530

    Added new Feature-c

--- main.cs
+++ main.cs
@@ -5,4 +5,7 @@ namespace DemoAPI
         Console.WriteLine("hello");
             Console.WriteLine("Changing the current file");
     }
+    public void FeatureC(){
+        Console.WriteLine("Added Feature-c");
+    }
 }
